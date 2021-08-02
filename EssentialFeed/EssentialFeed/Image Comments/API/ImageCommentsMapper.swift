048d99b420b3994a98c51f80fba0a1e795573037
Nicolas De Maio <nicolasddemaio@gmail.com>
2021-08-02 18:57:50 -0300

    Moving production code from test files to main project for ImageComment entities

---
+++ EssentialFeed/EssentialFeed/Image Comments/API/ImageCommentsMapper.swift
@@ -0,0 +1,41 @@
+//
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public class ImageCommentsMapper {
+	private struct Root: Decodable {
+		private let items: [RemoteImageComment]
+
+		private struct Author: Decodable {
+			let username: String
+		}
+
+		private struct RemoteImageComment: Decodable {
+			let id: UUID
+			let message: String
+			let created_at: Date
+			let author: Author
+		}
+
+		var comments: [ImageComment] {
+			items.map { ImageComment(id: $0.id, message: $0.message, createdAt: $0.created_at, author: $0.author.username) }
+		}
+	}
+
+	public enum Error: Swift.Error {
+		case invalidData
+	}
+
+	public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+		let decoder = JSONDecoder()
+		decoder.dateDecodingStrategy = .iso8601
+
+		guard response.isRangeOK, let root = try? decoder.decode(Root.self, from: data) else {
+			throw Error.invalidData
+		}
+
+		return root.comments
+	}
+}
--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
@@ -5,64 +5,6 @@
 import XCTest
 @testable import EssentialFeed
 
-struct ImageComment: Hashable {
-	public let id: UUID
-	public let message: String
-	public let createdAt: Date
-	public let author: String
-
-	public init(id: UUID, message: String, createdAt: Date, author: String) {
-		self.id = id
-		self.message = message
-		self.createdAt = createdAt
-		self.author = author
-	}
-}
-
-class ImageCommentsMapper {
-	private struct Root: Decodable {
-		private let items: [RemoteImageComment]
-
-		private struct Author: Decodable {
-			let username: String
-		}
-
-		private struct RemoteImageComment: Decodable {
-			let id: UUID
-			let message: String
-			let created_at: Date
-			let author: Author
-		}
-
-		var comments: [ImageComment] {
-			items.map { ImageComment(id: $0.id, message: $0.message, createdAt: $0.created_at, author: $0.author.username) }
-		}
-	}
-
-	public enum Error: Swift.Error {
-		case invalidData
-	}
-
-	public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
-		let decoder = JSONDecoder()
-		decoder.dateDecodingStrategy = .iso8601
-
-		guard response.isRangeOK, let root = try? decoder.decode(Root.self, from: data) else {
-			throw Error.invalidData
-		}
-
-		return root.comments
-	}
-}
-
-extension HTTPURLResponse {
-	private static var OK_2xx: Range<Int> { return 200 ..< 300 }
-
-	var isRangeOK: Bool {
-		return HTTPURLResponse.OK_2xx.contains(statusCode)
-	}
-}
-
 class ImageCommentsMapperTests: XCTestCase {
 	func test_map_throwsErrorOnNon2xxHTTPResponse() throws {
 		let json = makeItemsJSON([])
