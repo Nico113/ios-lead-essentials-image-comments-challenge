86001a6e1623d03ee5b68bea4f3c208c2f6a7308
Nicolas De Maio <nicolasddemaio@gmail.com>
2021-08-03 14:20:48 -0300

    ImageCommentsPresenter title is being localized

--- EssentialFeed/EssentialFeedTests/Image Comments/Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/Presentation/ImageCommentsPresenterTests.swift
@@ -5,9 +5,19 @@
 import XCTest
 @testable import EssentialFeed
 
+class ImageCommentsPresenter {
+	public static var title: String {
+		NSLocalizedString(
+			"IMAGE_COMMENTS_VIEW_TITLE",
+			tableName: "ImageComments",
+			bundle: Bundle(for: FeedPresenter.self),
+			comment: "Title for the image comments view")
+	}
+}
+
 class ImageCommentsPresenterTests: XCTestCase {
 	func test_title_isLocalized() {
-		XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+		XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
 	}
 
 	func test_map_createsViewModel() {
@@ -21,7 +31,7 @@ class ImageCommentsPresenterTests: XCTestCase {
 	// MARK: - Helpers
 
 	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
-		let table = "Feed"
+		let table = "ImageComments"
 		let bundle = Bundle(for: FeedPresenter.self)
 		let value = bundle.localizedString(forKey: key, value: nil, table: table)
 		if value == key {
