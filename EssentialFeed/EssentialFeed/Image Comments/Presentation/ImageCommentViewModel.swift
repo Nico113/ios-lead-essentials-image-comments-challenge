0b7bc0bfd14caa1082f5ddc4df5c2d517b2721bf
Nicolas De Maio <nicolasddemaio@gmail.com>
2021-08-10 18:29:55 -0300

    Adding ImageComments cell controller and its storyboard

---
+++ EssentialFeed/EssentialFeed/Image Comments/Presentation/ImageCommentViewModel.swift
@@ -0,0 +1,9 @@
+//
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+public struct ImageCommentViewModel {
+	public let author: String
+	public let date: String
+	public let comment: String
+}
